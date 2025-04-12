49ef70a5504f0dc730fd46afc7e3ca8770e8013e
heeae <gmldo2468@gmail.com>
2025-04-12 14:50:51 +0900

    QnaController 수정

--- BEAUTY_LOG/src/main/java/egovframework/beauty/front/controller/QnaController.java
+++ BEAUTY_LOG/src/main/java/egovframework/beauty/front/controller/QnaController.java
@@ -1,7 +1,5 @@
 package egovframework.beauty.front.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
@@ -17,7 +15,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import egovframework.beauty.front.model.QnaVO;
